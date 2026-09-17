e2860d36d867aff2dd52a76eceb4dea2c926ff0a
Gonzalo <gonzadjstaff@gmail.com>
2026-09-16 23:49:24 -0300

    Conexion con Supabase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SupabaseService } from './core/services/supabase';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.scss',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('cine-app');
+  private readonly supabase = inject(SupabaseService);
+
+  constructor() {
+    this.supabase.client.auth.getSession().then(({ data, error }) => {
+      console.log('Supabase conectado', { data, error });
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://sadkyjqyrjywvoqcovvl.supabase.co',
+    supabaseKey: 'Cine-app1984'
+};
